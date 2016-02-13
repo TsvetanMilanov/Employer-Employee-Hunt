c4e1a5ee64366145e1c0ce6de4e57070892953c1
TsvetanMilanov <tsvetan.milanov17@gmail.com>
2016-02-13 23:48:07 +0200

    Added statistics services.

---
+++ Services/EmployerEmployeeHuntSystem.Services.Data/IStatisticsServices.cs
@@ -0,0 +1,9 @@
+﻿namespace EmployerEmployeeHuntSystem.Services.Data
+{
+    using System.Collections.Generic;
+
+    public interface IStatisticsServices
+    {
+        IDictionary<string, int> GetFullStatistics();
+    }
+}
---
+++ Services/EmployerEmployeeHuntSystem.Services.Data/StatisticsServices.cs
@@ -0,0 +1,39 @@
+﻿namespace EmployerEmployeeHuntSystem.Services.Data
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using EmployerEmployeeHuntSystem.Data.Common;
+    using EmployerEmployeeHuntSystem.Data.Models;
+
+    public class StatisticsServices : IStatisticsServices
+    {
+        private IDbRepository<HeadhunterProfile, string> headHunters;
+        private IDbRepository<DeveloperProfile, string> developers;
+        private IDbRepository<Organization, int> organizations;
+        private IDbRepository<JobOffer, int> jobOffers;
+
+        public StatisticsServices(
+            IDbRepository<HeadhunterProfile, string> headHunters,
+            IDbRepository<DeveloperProfile, string> developers,
+            IDbRepository<Organization, int> organizations,
+            IDbRepository<JobOffer, int> jobOffers)
+        {
+            this.headHunters = headHunters;
+            this.developers = developers;
+            this.organizations = organizations;
+            this.jobOffers = jobOffers;
+        }
+
+        public IDictionary<string, int> GetFullStatistics()
+        {
+            Dictionary<string, int> result = new Dictionary<string, int>();
+
+            result.Add("Headhunters", this.headHunters.All().Count());
+            result.Add("Developers", this.headHunters.All().Count());
+            result.Add("Organizations", this.headHunters.All().Count());
+            result.Add("Job Offers", this.headHunters.All().Count());
+
+            return result;
+        }
+    }
+}
