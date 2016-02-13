f55e616a869a7a802151f43e336f2cebfdb33ab6
TsvetanMilanov <tsvetan.milanov17@gmail.com>
2016-02-13 23:47:50 +0200

    Added initial seed of all the roles and users for all roles.

--- Data/EmployerEmployeeHuntSystem.Data/Migrations/Configuration.cs
+++ Data/EmployerEmployeeHuntSystem.Data/Migrations/Configuration.cs
@@ -17,25 +17,78 @@
 
         protected override void Seed(EmployerEmployeeHuntDbContext context)
         {
-            const string AdministratorUserName = "admin@admin.com";
-            const string AdministratorPassword = AdministratorUserName;
+            this.SeedRoles(context);
+            this.SeedUsers(context);
+        }
 
+        private void SeedRoles(EmployerEmployeeHuntDbContext context)
+        {
             if (!context.Roles.Any())
             {
-                // Create admin role
                 var roleStore = new RoleStore<IdentityRole>(context);
                 var roleManager = new RoleManager<IdentityRole>(roleStore);
-                var role = new IdentityRole { Name = GlobalConstants.AdministratorRoleName };
-                roleManager.Create(role);
 
-                // Create admin user
+                var adminRole = new IdentityRole { Name = GlobalConstants.AdministratorRoleName };
+                var headhunterRole = new IdentityRole { Name = GlobalConstants.HeadhunterRoleName };
+                var userRole = new IdentityRole { Name = GlobalConstants.UserRoleName };
+                roleManager.Create(adminRole);
+                roleManager.Create(headhunterRole);
+                roleManager.Create(userRole);
+            }
+        }
+
+        private void SeedUsers(EmployerEmployeeHuntDbContext context)
+        {
+            if (!context.Users.Any())
+            {
+                const string AdministratorUserName = "admin@admin.com";
+                const string AdministratorPassword = AdministratorUserName;
+
+                const string HeadhunterUserName = "headhunter@headhunter.com";
+                const string HeadhunterPassword = HeadhunterUserName;
+
+                const string DeveloperUserName = "developer@developer.com";
+                const string DeveloperPassword = DeveloperUserName;
+
+                const string EmployerUserName = "employer@employer.com";
+                const string EmployerPassword = EmployerUserName;
+
                 var userStore = new UserStore<User>(context);
                 var userManager = new UserManager<User>(userStore);
-                var user = new User { UserName = AdministratorUserName, Email = AdministratorUserName };
-                userManager.Create(user, AdministratorPassword);
 
-                // Assign user to admin role
-                userManager.AddToRole(user.Id, GlobalConstants.AdministratorRoleName);
+                var adminUser = new User
+                {
+                    UserName = AdministratorUserName,
+                    Email = AdministratorUserName
+                };
+
+                var headhunterUser = new User
+                {
+                    UserName = HeadhunterUserName,
+                    Email = HeadhunterUserName
+                };
+
+                var developerUser = new User
+                {
+                    UserName = DeveloperUserName,
+                    Email = DeveloperUserName
+                };
+
+                var employerUser = new User
+                {
+                    UserName = EmployerUserName,
+                    Email = EmployerUserName
+                };
+
+                userManager.Create(adminUser, AdministratorPassword);
+                userManager.Create(headhunterUser, HeadhunterPassword);
+                userManager.Create(developerUser, DeveloperPassword);
+                userManager.Create(employerUser, EmployerPassword);
+
+                userManager.AddToRole(adminUser.Id, GlobalConstants.AdministratorRoleName);
+                userManager.AddToRole(headhunterUser.Id, GlobalConstants.HeadhunterRoleName);
+                userManager.AddToRole(developerUser.Id, GlobalConstants.UserRoleName);
+                userManager.AddToRole(employerUser.Id, GlobalConstants.UserRoleName);
             }
         }
     }
