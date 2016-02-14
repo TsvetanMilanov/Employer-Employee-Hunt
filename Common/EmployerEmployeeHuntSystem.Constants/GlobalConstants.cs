e431739ff87664da890158026d0613cc8e69169d
TsvetanMilanov <tsvetan.milanov17@gmail.com>
2016-02-14 13:22:21 +0200

    Added global success / error message handling.

--- Common/EmployerEmployeeHuntSystem.Constants/GlobalConstants.cs
+++ Common/EmployerEmployeeHuntSystem.Constants/GlobalConstants.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EmployerEmployeeHuntSystem.Constants
+﻿namespace EmployerEmployeeHuntSystem.Constants
 {
     public class GlobalConstants
     {
         public const string AdministratorRoleName = "Administrator";
-
         public const string HeadhunterRoleName = "Headhunter";
-
         public const string UserRoleName = "User";
+
+        public const string SuccessMessageTempDataKey = "SuccessMessageTempData";
+        public const string ErrorMessageTempDataKey = "ErrorMessageTempData";
     }
 }
