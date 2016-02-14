2ae2943d35d59635e3ba7567e9963058236dddd9
TsvetanMilanov <tsvetan.milanov17@gmail.com>
2016-02-14 11:24:13 +0200

    Added service for organizations.

---
+++ Services/EmployerEmployeeHuntSystem.Services.Data/OrganizationsService.cs
@@ -0,0 +1,22 @@
+﻿namespace EmployerEmployeeHuntSystem.Services.Data
+{
+    using System;
+    using System.Linq;
+    using EmployerEmployeeHuntSystem.Data.Common;
+    using EmployerEmployeeHuntSystem.Data.Models;
+
+    public class OrganizationsService : IOrganizationsService
+    {
+        private IDbRepository<Organization, int> organizations;
+
+        public OrganizationsService(IDbRepository<Organization, int> organizations)
+        {
+            this.organizations = organizations;
+        }
+
+        public IQueryable<Organization> GetAll()
+        {
+            return this.organizations.All();
+        }
+    }
+}
