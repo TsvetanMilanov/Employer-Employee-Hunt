c912b16b87d4c1c46e6044ba7831f47f7622a4e9
TsvetanMilanov <tsvetan.milanov17@gmail.com>
2016-02-16 01:54:42 +0200

    Added headhunter control panel view structure.

---
+++ Web/EmployerEmployeeHuntSystem.Web/Areas/Headhunter/Controllers/JobOffersController.cs
@@ -0,0 +1,13 @@
+﻿namespace EmployerEmployeeHuntSystem.Web.Areas.Headhunter.Controllers
+{
+    using System.Web.Mvc;
+    using ViewModels;
+
+    public class JobOffersController : HeadhuntersBaseController
+    {
+        public ActionResult Current()
+        {
+            return this.View(new BaseViewModel());
+        }
+    }
+}
