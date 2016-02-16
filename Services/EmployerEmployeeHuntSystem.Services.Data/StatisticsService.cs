adc0b9c7ba5c6a8bf1993a482af175960ec0bd08
TsvetanMilanov <tsvetan.milanov17@gmail.com>
2016-02-16 08:36:14 +0200

    Fixed the calculation of headhunters count in the statistics service.

--- Services/EmployerEmployeeHuntSystem.Services.Data/StatisticsService.cs
+++ Services/EmployerEmployeeHuntSystem.Services.Data/StatisticsService.cs
@@ -2,9 +2,11 @@
 {
     using System.Collections.Generic;
     using System.Linq;
+    using Constants;
     using Contracts;
     using EmployerEmployeeHuntSystem.Data.Common;
     using EmployerEmployeeHuntSystem.Data.Models;
+    using Microsoft.AspNet.Identity.EntityFramework;
 
     public class StatisticsService : IStatisticsService
     {
@@ -12,17 +14,23 @@
         private IDbRepository<DeveloperProfile, string> developers;
         private IDbRepository<Organization, int> organizations;
         private IDbRepository<JobOffer, int> jobOffers;
+        private IGenericRepository<User> users;
+        private IGenericRepository<IdentityRole> roles;
 
         public StatisticsService(
             IDbRepository<HeadhunterProfile, string> headHunters,
             IDbRepository<DeveloperProfile, string> developers,
             IDbRepository<Organization, int> organizations,
-            IDbRepository<JobOffer, int> jobOffers)
+            IDbRepository<JobOffer, int> jobOffers,
+            IGenericRepository<User> users,
+            IGenericRepository<IdentityRole> roles)
         {
             this.headHunters = headHunters;
             this.developers = developers;
             this.organizations = organizations;
             this.jobOffers = jobOffers;
+            this.users = users;
+            this.roles = roles;
         }
 
         public IDictionary<string, int> GetFullStatistics()
@@ -32,7 +40,9 @@
             result.Add("Organizations", this.organizations.All().Count());
             result.Add("Job Offers", this.jobOffers.All().Count());
             result.Add("Developers", this.developers.All().Count());
-            result.Add("Headhunters", this.headHunters.All().Count());
+
+            var headhunterRoleId = this.roles.All().FirstOrDefault(r => r.Name == GlobalConstants.HeadhunterRoleName).Id;
+            result.Add("Headhunters", this.users.All().Count(u => u.Roles.Any(r => r.RoleId == headhunterRoleId)));
 
             return result;
         }
