cb28f67fb20a4fc959fdc45aac2e6df65942571b
TsvetanMilanov <tsvetan.milanov17@gmail.com>
2016-02-24 11:34:22 +0200

    Added route tests for the candidacies.

---
+++ Tests/EmployerEmployeeHuntSystem.Web.Routes.Tests/CandidaciesTests.cs
@@ -0,0 +1,38 @@
+﻿namespace EmployerEmployeeHuntSystem.Web.Routes.Tests
+{
+    using System.Web.Routing;
+    using Controllers;
+    using MvcRouteTester;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class CandidaciesTests
+    {
+        private RouteCollection routeCollection;
+
+        [TestFixtureSetUp]
+        public void Init()
+        {
+            this.routeCollection = new RouteCollection();
+            RouteConfig.RegisterRoutes(this.routeCollection);
+        }
+
+        [Test]
+        public void ApproveRouteShouldMapCorrectly()
+        {
+            int id = 5;
+            string route = string.Format("/Candidacies/Approve/{0}", id);
+
+            this.routeCollection.ShouldMap(route).To<CandidaciesController>(c => c.Approve(id));
+        }
+
+        [Test]
+        public void DeleteRouteShouldMapCorrectly()
+        {
+            int id = 5;
+            string route = string.Format("/Candidacies/Delete/{0}", id);
+
+            this.routeCollection.ShouldMap(route).To<CandidaciesController>(c => c.Delete(id));
+        }
+    }
+}
